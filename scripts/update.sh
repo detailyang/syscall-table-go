695f32257896ca90f938bd6f1ee84f8799260807
detailyang <detailyang@gmail.com>
2024-05-15 17:00:51 +0800

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,4 +8,7 @@ var Table map[uint64]string = map[uint64]string{
 $(curl https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_64.tbl | grep -v '\#' |  grep -v "^\$"|  awk '{print $1 ":""\""$3"\","}')
 }
 
+var SysTable map[string]uint64 = map[string]uint64{
+$(curl https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_64.tbl | grep -v '\#' |  grep -v "^\$"|  awk '{print "\""$3"\"" ":"$1","}')
+}
 EOS
